659633156c362254310d8a694d7f79e450ca8722
igor <shevchenkcoigor@gmail.com>
2020-07-10 19:08:11 +0300

    SCRAPER_ETSY-59 Item table use pagination

--- frontend/src/components/Item/Item.js
+++ frontend/src/components/Item/Item.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {ReactTinyLink} from "react-tiny-link";
-import {makeStyles} from "@material-ui/core/styles";
+import TableContainer from '@material-ui/core/TableContainer';
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
@@ -11,78 +11,194 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import TreeView from '@material-ui/lab/TreeView';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import TreeItem from '@material-ui/lab/TreeItem';
+import {lighten, makeStyles} from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import clsx from 'clsx';
+import TablePagination from '@material-ui/core/TablePagination';
 import styles from "assets/jss/material-dashboard-react/components/tableStyle.js";
 
 const useStyles = makeStyles(styles);
 
+const useToolbarStyles = makeStyles((theme) => ({
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+  },
+  highlight:
+      theme.palette.type === 'light'
+          ? {
+            color: theme.palette.secondary.main,
+            backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+          }
+          : {
+            color: theme.palette.text.primary,
+            backgroundColor: theme.palette.secondary.dark,
+          },
+  title: {
+    flex: '1 1 100%',
+  },
+}));
+
+const EnhancedTableToolbar = (props) => {
+  const classes = useToolbarStyles();
+  const { numSelected } = props;
+
+  return (
+      <Toolbar
+          className={clsx(classes.root, {
+            [classes.highlight]: numSelected > 0,
+          })}
+      >
+        {numSelected > 0 ? (
+            <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
+              {numSelected} selected
+            </Typography>
+        ) : (
+            <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
+              Nutrition
+            </Typography>
+        )}
+
+        {numSelected > 0 ? (
+            <Tooltip title="Delete">
+              <IconButton aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
+            </Tooltip>
+        ) : (
+            <Tooltip title="Filter list">
+              <IconButton aria-label="filter list">
+                <FilterListIcon />
+              </IconButton>
+            </Tooltip>
+        )}
+      </Toolbar>
+  );
+};
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+function EnhancedTableHead(props) {
+  const { classes, tableHead } = props;
+
+  return (
+      <TableHead>
+        <TableRow>
+          {tableHead.map((headCell, key) => (
+              <TableCell
+                  key={key}
+                  // classes.tableCell + " " + classes.tableHeadCell
+                  // align={headCell.numeric ? 'right' : 'left'}
+                  // padding={headCell.disablePadding ? 'none' : 'default'}
+              >
+                {headCell}
+              </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+  );
+}
+
 export default function ItemTable(props) {
   const classes = useStyles();
+  const { tableHead, tableData } = props;
 
-  const { tableHead, tableData, tableHeaderColor } = props;
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [dense] = React.useState(false);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, tableData.length - page * rowsPerPage);
+
   return (
       <div className={classes.tableResponsive}>
-        <Table className={classes.table}>
-          {tableHead !== undefined ? (
-              <TableHead className={classes[tableHeaderColor + "TableHeader"]}>
-                <TableRow className={classes.tableHeadRow}>
-                  {tableHead.map((prop, key) => {
-                    return (
-                        <TableCell
-                            className={classes.tableCell + " " + classes.tableHeadCell}
-                            key={key}
-                        >
-                          {prop}
-                        </TableCell>
-                    );
-                  })}
-                </TableRow>
-              </TableHead>
-          ) : null}
-          <TableBody>
-            {tableData.map((item) => {
-              return (
-                  <TableRow key={item.id.toString()} className={classes.tableBodyRow}>
-                    <TableCell className={classes.tableCell}>
-                      <TreeView
-                          className={classes.root}
-                          defaultCollapseIcon={<ExpandMoreIcon />}
-                          defaultExpandIcon={<ChevronRightIcon />}
-                      >
-                        <TreeItem nodeId={item.id.toString()} label="Tags">
-                          {item.tags.map((tag) => {
-                            return (
-                                <TreeItem
-                                    nodeId={tag.id.toString()}
-                                    key={tag.id.toString()}
-                                    label={tag.name} />
-                            );
-                          })}
-                        </TreeItem>
-                      </TreeView>
-                    </TableCell>
-                    <TableCell className={classes.tableCell}>
-                      <ReactTinyLink
-                          cardSize="small"
-                          showGraphic={true}
-                          maxLine={2}
-                          minLine={1}
-                          url={ item.url }
-                      />
-                    </TableCell>
-                    <TableCell className={classes.tableCell}>
-                      <ReactTinyLink
-                          cardSize="small"
-                          showGraphic={true}
-                          maxLine={2}
-                          minLine={1}
-                          url={ item.shop_url }
-                      />
-                    </TableCell>
-                  </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
+        <Paper className={classes.paper}>
+          <EnhancedTableToolbar />
+          <TableContainer>
+            <Table className={classes.table}>
+              <EnhancedTableHead
+                  classes={classes}
+                  tableHead={tableHead}
+              />
+              <TableBody>
+                {tableData
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((item) => {
+                      return (
+                          <TableRow key={item.id.toString()} className={classes.tableBodyRow}>
+                            <TableCell className={classes.tableCell}>
+                              <TreeView
+                                  className={classes.root}
+                                  defaultCollapseIcon={<ExpandMoreIcon />}
+                                  defaultExpandIcon={<ChevronRightIcon />}
+                              >
+                                <TreeItem nodeId={item.id.toString()} label="Tags">
+                                  {item.tags.map((tag) => {
+                                    return (
+                                        <TreeItem
+                                            nodeId={tag.id.toString()}
+                                            key={tag.id.toString()}
+                                            label={tag.name} />
+                                    );
+                                  })}
+                                </TreeItem>
+                              </TreeView>
+                            </TableCell>
+                            <TableCell className={classes.tableCell}>
+                              <ReactTinyLink
+                                  cardSize="small"
+                                  showGraphic={true}
+                                  maxLine={2}
+                                  minLine={1}
+                                  url={ item.url }
+                              />
+                            </TableCell>
+                            <TableCell className={classes.tableCell}>
+                              <ReactTinyLink
+                                  cardSize="small"
+                                  showGraphic={true}
+                                  maxLine={2}
+                                  minLine={1}
+                                  url={ item.shop_url }
+                              />
+                            </TableCell>
+                          </TableRow>
+                      );
+                    })}
+                {emptyRows > 0 && (
+                    <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component="div"
+              count={tableData.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+          />
+        </Paper>
       </div>
   );
 }
