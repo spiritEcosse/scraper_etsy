5eac792f414451626e09b48d15de4ef2abd0fd69
igor <shevchenkcoigor@gmail.com>
2020-07-28 22:38:24 +0300

    SCRAPER_ETSY-99 make deploy with CI

--- config/settings/production.py
+++ config/settings/production.py
@@ -172,7 +172,7 @@ sentry_sdk.init(
 
 # we whitelist localhost:3000 because that's where frontend will be served
 CORS_ORIGIN_WHITELIST = (
-    "http://127.0.0.1:80",
+    "http://127.0.0.1:8240",
 )
 
 COUNTDOWN = 5
